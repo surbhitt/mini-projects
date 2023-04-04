ef7539a251d850045e1fe3249de8df406c2c3df1
savaskaranli <surbhit.thakur@gmail.com>
2023-04-04 12:35:27 +0530

    faulty compression algo

--- TerminalAsciiGraphics/imgascii.c
+++ TerminalAsciiGraphics/imgascii.c
@@ -4,6 +4,14 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "header/stb_image.h"
 
+#define SCALE_DOWN_FAC 16
+#define PIXEL_R(color) (((color)&0x000000ff)>>(8*0))
+#define PIXEL_G(color) (((color)&0x0000ff00)>>(8*1))
+#define PIXEL_B(color) (((color)&0x00ff0000)>>(8*2))
+#define PIXEL_A(color) (((color)&0xff000000)>>(8*3))
+#define RECOMP_RGBA(r, g, b, a) ((((r)&0xff)<<(8*0)) | (((g)&0xff)<<(8*1)) | (((b)&0xff)<<(8*2)) | (((a)&0xff)<<(8*3))) 
+
+
 /****************
 1) Load the image
 2) Scale down the pixelchunk loaded:
@@ -16,17 +24,51 @@
 
 ****************/
 
-uint32_t *scaled_down(uint32_t *pixels) {
-	return pixels;	
+uint32_t *scaled_down(uint32_t *pixels, int height, int width) {
+
+	uint32_t *pixels_tr;
+	int i=0,j=0,cnt=0;
+	uint32_t pixel;
+	for (;;) {
+		uint32_t tmpr=0, tmpg=0, tmpb=0, tmpa=0;
+		for (int x=0;x<SCALE_DOWN_FAC;++x) {
+			for (int y=0; y<SCALE_DOWN_FAC; ++y) {
+				pixel = pixels[(i*width + j) + (x*width + y)];
+				tmpr += PIXEL_R(pixel);
+				tmpg += PIXEL_G(pixel);
+				tmpb += PIXEL_B(pixel);
+				tmpa += PIXEL_A(pixel);
+			}
+		}
+		uint32_t tr_r = tmpr/SCALE_DOWN_FAC*SCALE_DOWN_FAC; 
+		uint32_t tr_g = tmpg/SCALE_DOWN_FAC*SCALE_DOWN_FAC;
+		uint32_t tr_b = tmpb/SCALE_DOWN_FAC*SCALE_DOWN_FAC;  
+		uint32_t tr_a = tmpa/SCALE_DOWN_FAC*SCALE_DOWN_FAC;
+		*(pixels_tr+cnt) = RECOMP_RGBA(tr_r, tr_g, tr_b, tr_a);
+		cnt++;
+		j += SCALE_DOWN_FAC;
+		if (j>=width) { 
+			i += SCALE_DOWN_FAC;
+			if (i>=height) {
+				break;
+			}
+			else {
+				j=0;
+			}
+		}
+	}
+	return pixels_tr;	
 }
 
 
 char table[] = ".,*:o#@";
 size_t n = sizeof(table) - 1;
+
+// changes the pixel to ascii char
 char color_to_char(uint32_t pixel) {
-	int r = (pixel >> 8*0) & 0xff;	
-	int g = (pixel >> 8*1) & 0xff;	
-	int b = (pixel >> 8*2) & 0xff;
+	int r = PIXEL_R(pixel);	
+	int g = PIXEL_G(pixel); 	
+	int b = PIXEL_B(pixel);
 	
 	size_t bright = r;
 	if (bright < g) bright = g;
@@ -38,6 +80,7 @@ char color_to_char(uint32_t pixel) {
 	return table[bright*n/256];
 }
 
+
 int main(int argc, char** argv) {
 	//printf("hello, subroza\n");
 	
@@ -45,20 +88,28 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "[ERROR] no input provided\n");
 		exit(1);
 	}
+
 	const char *file_path = argv[1];
+	
 	int width, height;
 	uint32_t *pixels = (uint32_t*)stbi_load(file_path, &width, &height, NULL, 4);
+	
 	if (pixels == NULL) {
 		fprintf(stderr, "[ERROR] file not able to load\n");
 		exit(1);
 	}
+	/*if (width%SCALE_DOWN_FAC != 0 && height%SCALE_DOWN_FAC != 0) {
+		fprintf(stderr, "[ERROR] width and height unscalable");
+	}*/
 	
-	uint32_t *pixels_tr = scaled_down(pixels);
-	for (int y=0 ; y<height; ++y) {
-		for (int x=0; x<width; ++x) {
+	uint32_t *pixels_tr = scaled_down(pixels, height, width);
+	int scaled_width = width / SCALE_DOWN_FAC;	
+	int scaled_height = height / SCALE_DOWN_FAC;
+	for (int y=0 ; y<scaled_height; ++y) {
+		for (int x=0; x<scaled_width; ++x) {
 			putc(color_to_char(pixels_tr[y*width + x]), stdout);
 		}
-		putc("\n", stdout);
+		printf("\n");
 	}
 
 	return 0;
